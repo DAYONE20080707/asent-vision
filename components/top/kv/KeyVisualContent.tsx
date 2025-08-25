80660f42b7160355527951ec6d97e9115d30015c
akicouleur <165789262+akicouleur@users.noreply.github.com>
2025-08-26 07:54:40 +0900

    画像差し替え

--- components/top/kv/KeyVisualContent.tsx
+++ components/top/kv/KeyVisualContent.tsx
@@ -1,6 +1,5 @@
 // components/ui/frame/KeyVisualContent.tsx
 import classNames from "classnames"
-import Image from "next/image"
 
 interface KeyVisualContentProps {
   className?: string
@@ -9,13 +8,13 @@ interface KeyVisualContentProps {
 const KeyVisualContent: React.FC<KeyVisualContentProps> = ({ className }) => {
   return (
     <div className={classNames("relative w-full", className)}>
-      <Image
-        src="/top/kv/kv_img1.jpg"
-        alt="Key Visual"
-        layout="fill"
-        objectFit="cover"
-        priority
-        className="object-center"
+      <video
+        src="/top/kv/kv.mp4"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="w-full h-full object-cover object-center"
       />
     </div>
   )
